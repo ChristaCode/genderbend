94095c7bef784c65f875001ff449e3b08756a2db
Christa Cooke <christacooke@Kristins-MacBook-Pro.local>
2020-11-13 12:38:22 +0000

    upload button

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const textract = require('textract');
 const app = express();
 const port = process.env.PORT || 5000;
+var multer = require('multer')
+var cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+  cb(null, 'public')
+},
+filename: function (req, file, cb) {
+  cb(null, Date.now() + '-' +file.originalname )
+}
+})
+
+var upload = multer({ storage: storage }).single('file')
 
 // API calls
 app.get('/api/hello', (req, res) => {
@@ -14,10 +29,20 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.post('/api/world', (req, res) => {
+
   console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+  // res.send(
+  //   `I received your POST request. This is what you sent me: ${req.body.post}`,
+  // );
+
+  upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        } else if (err) {
+            return res.status(500).json(err)
+        }
+    return res.status(200).send(req.file)
+  });
 });
 
 if (process.env.NODE_ENV === 'production') {
