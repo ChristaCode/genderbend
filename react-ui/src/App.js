f1711556a276fc5f7808893de7dbc170067cfa32
Christa Cooke <christacooke@Kristins-MacBook-Pro.local>
2020-11-15 05:18:06 +0000

    just rearrange the whole thing

---
+++ react-ui/src/App.js
@@ -0,0 +1,96 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import genderIcon from './genderIcon.png';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [url, setUrl] = useState('/api');
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [parsing, setParsing] = useState(false);
+  const [converted, setConverted] = useState(false);
+
+  const onChangeHandler=event=>{
+    setSelectedFile(event.target.files[0]);
+  }
+
+  const onClickHandler = async e => {
+    e.preventDefault();
+    setParsing(true);
+
+    const data = new FormData()
+    data.append('file', selectedFile)
+    axios.post('/api/upload', data, {})
+    .then()
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(() => setParsing(false))
+    .then(() => setConverted(true))
+  }
+
+  const onDownloadClickHandler = async () => {
+    console.log('selectedFile', selectedFile.name);
+    const response = await fetch('/api/download');
+    const element = document.createElement("a");
+    const blob = await response.blob();
+    element.href = URL.createObjectURL(blob);
+    element.download = 'converted-' + selectedFile.name;
+    document.body.appendChild(element);
+    element.click();
+  }
+
+  const fetchData = useCallback(() => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        setMessage(json.message);
+        setIsFetching(false);
+      }).catch(e => {
+        setMessage(`API call failed: ${e}`);
+        setIsFetching(false);
+      })
+  }, [url]);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={genderIcon} alt="symbol" style={{width: 50, height: 50, padding: 5}} />
+        <div className="headerTitle">GenderBend</div>
+      </header>
+      <br />
+      <div>
+        <br />
+        <input className="inputStyle" type="file" onChange={onChangeHandler} accept=".txt"/>
+        <button className="buttonStyle" type="button" onClick={onClickHandler}>Convert</button>
+        <div>
+          {parsing &&
+            <p class="loading">loading</p>}
+        </div>
+      </div>
+      <br />
+      <div>
+        {converted &&
+          <button className="buttonStyle" type="button" onClick={onDownloadClickHandler}>
+            Download
+          </button>
+        }
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+const replace = require('replace-in-file');
+var multer = require('multer')
+const fs = require('fs');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
+
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+
+} else {
+  const app = express();
+
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+
+  // multer
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, '')
+  },
+  filename: function (req, file, cb) {
+    fileName = file.originalname;
+    cb(null, './uploadedFile/' + 'genderbend-'+fileName)
+  }
+  })
+
+  var upload = multer({ storage: storage }).single('file');
+
+  // cleanup converted files
+  const cleanup = () => {
+    const files = fs.readdirSync('./uploadedFile');
+    if (files.length > 0) {
+      files.forEach(function(filename) {
+        fs.unlinkSync('./uploadedFile/' + filename);
+      })
+    }
+  }
+
+  app.post('/api/upload', async (req, res) => {
+    upload(req, res, async () => {
+      const options = {
+        files: './uploadedFile/*',
+        from: [/he/g, /him/g, /his/g, /himself/g, /brother/g, /nephew/g, /uncle/g],
+        to: ['her', 'her', 'hers', 'herself', 'sister', 'niece', 'aunt'],
+      };
+    
+      try {
+        const results = await replace(options)
+        console.log('Replacement results:', results);
+        return res.status(200).send(req.file);
+      }
+      catch (error) {
+        console.error('Error occurred:', error);
+      }
+    })
+  });
+  
+  app.get('/api/download', (req, res) => {
+    const filename = fs.readdirSync('./uploadedFile');
+    const folderPath = './uploadedFile/' + filename;
+    res.download(folderPath, null, () => {
+      cleanup();
+    });
+  });
+
+  // Answer API requests.
+  app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+  });
+
+
+
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  });
+
+  app.listen(PORT, function () {
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  });
+}
